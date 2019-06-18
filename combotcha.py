b0dd9bc17d1210fec1c58fcfa7e690b9268cfb0a
Jérémie Galarneau <jeremie.galarneau@gmail.com>
2019-06-18 13:41:39 -0400

    Clean-up: reformat combotcha.py using black
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@gmail.com>

--- combotcha.py
+++ combotcha.py
@@ -33,6 +33,7 @@ import tempfile
 import glob
 import git
 
+
 class Repository:
     def __init__(self, name, url, last_seen_commit_sha=None):
         self._name = name
@@ -41,9 +42,9 @@ class Repository:
         self._repo = None
         self._last_seen_commit_sha = last_seen_commit_sha
 
-        print('Cloning ' + self._name)
+        print("Cloning " + self._name)
         git.Git(self._directory.name).clone(self._url)
-        self._repo = git.Repo(glob.glob(self._directory.name + '/*/')[0])
+        self._repo = git.Repo(glob.glob(self._directory.name + "/*/")[0])
 
     @property
     def name(self):
@@ -51,7 +52,7 @@ class Repository:
 
     def get_new_commits(self):
         if self._last_seen_commit_sha is None:
-            for commit in self._repo.iter_commits('origin/master'):
+            for commit in self._repo.iter_commits("origin/master"):
                 self._last_seen_commit_sha = commit.hexsha
                 return []
         try:
@@ -62,7 +63,7 @@ class Repository:
             return []
 
         new_commits = []
-        for commit in self._repo.iter_commits('origin/master'):
+        for commit in self._repo.iter_commits("origin/master"):
             if commit.hexsha.startswith(self._last_seen_commit_sha):
                 break
             new_commits.append(commit)
@@ -76,18 +77,18 @@ class IRCSession:
         pass
 
     def __init__(self, server_url, server_port, channel_name, nickname):
-        print('Connecting to {}...'.format(server_url))
+        print("Connecting to {}...".format(server_url))
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((server_url, server_port))
         self._sock_send_lock = threading.Lock()
         self._sock = sock
         self._channel = channel_name
-        self._status_pattern = re.compile('^.* ([0-9][0-9][0-9]) .*')
+        self._status_pattern = re.compile("^.* ([0-9][0-9][0-9]) .*")
         self._reception_buffer = bytearray()
         self._nickname = nickname
 
     def _send_command(self, cmd_name, cmd_payload):
-        payload = '{} {}\r\n'.format(cmd_name, cmd_payload).encode('utf-8')
+        payload = "{} {}\r\n".format(cmd_name, cmd_payload).encode("utf-8")
         self._sock_send_lock.acquire()
         try:
             self._sock.send(payload)
@@ -95,26 +96,26 @@ class IRCSession:
             self._sock_send_lock.release()
 
     def _identify(self, nickname):
-        print('Identifying as ' + nickname)
-        self._send_command('NICK', nickname)
-        self._send_command('USER', '{} * * :{}'.format(nickname, nickname))
+        print("Identifying as " + nickname)
+        self._send_command("NICK", nickname)
+        self._send_command("USER", "{} * * :{}".format(nickname, nickname))
 
     def _join_channel(self, channel):
-        print('Joining ' + channel)
-        self._send_command('JOIN', channel)
+        print("Joining " + channel)
+        self._send_command("JOIN", channel)
         self._channel = channel
 
     def message_channel(self, msg):
-        self._send_command('PRIVMSG ' + self._channel, ':' + msg)
+        self._send_command("PRIVMSG " + self._channel, ":" + msg)
 
     def _pop_message(self):
         msg = None
         if len(self._reception_buffer) > 1:
             for i in range(1, len(self._reception_buffer)):
                 # Look for a message boundary (\r\n)
-                if self._reception_buffer[i - 1:i + 1] == b'\r\n':
-                    msg = self._reception_buffer[:i - 1].decode('utf-8')
-                    del self._reception_buffer[:i + 1]
+                if self._reception_buffer[i - 1 : i + 1] == b"\r\n":
+                    msg = self._reception_buffer[: i - 1].decode("utf-8")
+                    del self._reception_buffer[: i + 1]
                     break
         return msg
 
@@ -128,10 +129,10 @@ class IRCSession:
 
     def _pong(self, payload):
         reply = payload.split(":")[1]
-        self._send_command('PONG', ':' + reply)
+        self._send_command("PONG", ":" + reply)
 
     def quit(self):
-        self._send_command('QUIT', ':Bye-bye-bye-bye-bye-bye-bye!!')
+        self._send_command("QUIT", ":Bye-bye-bye-bye-bye-bye-bye!!")
 
     def _get_payload_status(self, payload):
         status = None
@@ -150,7 +151,7 @@ class IRCSession:
             if "No Ident response" in payload:
                 self._identify(self._nickname)
             elif status == 433:
-                print('Nickname already in use... BYYYYYYE!')
+                print("Nickname already in use... BYYYYYYE!")
                 session.quit()
                 raise self.NicknameInUse
             elif status == 376:
@@ -159,7 +160,7 @@ class IRCSession:
                 break
 
     def _handle_message(self, payload):
-        if 'PING' in payload:
+        if "PING" in payload:
             self._pong(payload)
 
     def run(self):
@@ -168,23 +169,25 @@ class IRCSession:
 
 
 if len(sys.argv) != 2:
-    print('Usage: combotcha config.json')
+    print("Usage: combotcha config.json")
     sys.exit(1)
 
 cfg = None
 with open(sys.argv[1]) as cfg_file:
     cfg = json.load(cfg_file)
 
-irc_cfg = cfg['irc']
-session = IRCSession(irc_cfg['url'], irc_cfg['port'], irc_cfg['channel'],
-                     irc_cfg['nick'])
+irc_cfg = cfg["irc"]
+session = IRCSession(
+    irc_cfg["url"], irc_cfg["port"], irc_cfg["channel"], irc_cfg["nick"]
+)
 session.sign_in()
 
 repos = []
-repos_cfg = cfg['repos']
+repos_cfg = cfg["repos"]
 for repo_cfg in repos_cfg:
-    repo = Repository(repo_cfg['name'], repo_cfg['url'],
-                      repo_cfg.get('last_seen_commit_sha', None))
+    repo = Repository(
+        repo_cfg["name"], repo_cfg["url"], repo_cfg.get("last_seen_commit_sha", None)
+    )
     repos.append(repo)
 
 
@@ -192,9 +195,10 @@ def sigint_handler(sig, frame):
     session.quit()
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, sigint_handler)
 
-print('Launching IRC thread')
+print("Launching IRC thread")
 irc_thread = threading.Thread(target=session.run)
 irc_thread.start()
 
@@ -204,10 +208,14 @@ while True:
         if not new_commits:
             continue
 
-        print('{} new commits found for {} repository'.format(
-            len(new_commits), repo.name))
-        session.message_channel('{} ({})'.format(repo.name, len(new_commits)))
+        print(
+            "{} new commits found for {} repository".format(len(new_commits), repo.name)
+        )
+        session.message_channel("{} ({})".format(repo.name, len(new_commits)))
         for commit in new_commits:
-            session.message_channel('\x0307{} \x0300{} \x0303[{}]'.format(
-                commit.hexsha[:8], commit.summary, commit.author.name))
+            session.message_channel(
+                "\x0307{} \x0300{} \x0303[{}]".format(
+                    commit.hexsha[:8], commit.summary, commit.author.name
+                )
+            )
     time.sleep(10)
